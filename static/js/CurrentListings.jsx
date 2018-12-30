65b13b190f59e22a0cf2cfc13b8233c0ee6aa229
Isaiah Weaver <iswea1@morgan.edu>
2018-12-29 22:36:15 -0500

    added CurrentListings component

---
+++ static/js/CurrentListings.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+export default class CurrentListings extends React.Component{
+  render (){
+    return <div>
+            <h3> <b>Current Listings</b></h3>
+            <p> $ 5 per week per box-bag </p>
+
+            <p> Contact Information:
+              (443)515-7044
+              warenoir@gmail.com
+
+            </p>
+          </div>
+
+  }
+}
--- static/js/HomePage.jsx
+++ static/js/HomePage.jsx
@@ -2,12 +2,14 @@ import React from "react"
 import ReactDOM from 'react-dom';
 import Summary from './Summary';
 import App from './App';
+import CurrentListings from './CurrentListings';
 class HomePage extends React.Component {
     render(){
       return (
         <div>
         <App />
         <Summary />
+        <CurrentListings />
         </div>
 
       );
