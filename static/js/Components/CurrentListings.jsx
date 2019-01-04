fdb5bca639966aa3f4881ed7b2672e0652561de5
Isaiah Weaver <iswea1@morgan.edu>
2019-01-04 17:04:04 -0500

    added props

--- static/js/Components/CurrentListings.jsx
+++ static/js/Components/CurrentListings.jsx
@@ -5,7 +5,10 @@ export default class CurrentListings extends React.Component{
   render (){
     return <div className="listings">
               <h2> Current Listings </h2>
-              <ListingsCard />
+              <ListingsCard
+                listing={{imgUrl:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTf7WxfwOSq6dfa6f9k617Z6UgC_UjDiBGOjBQjvEfas9gUUIch",
+              phone:"(443) 515-7044",email:"warenoir@gmail.com"}}
+              />
           </div>
 
   }
--- static/js/Components/ListingsCard.jsx
+++ static/js/Components/ListingsCard.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 function ListingsCard (props){
   return(
     <div className="listings-card">
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTf7WxfwOSq6dfa6f9k617Z6UgC_UjDiBGOjBQjvEfas9gUUIch"/>
+        <img src={props.listing.imgUrl}/>
         <p>Area: Towson, MD</p>
         <p>Long Term Storage: $10 per week per bag</p>
         <p>Short Term Storage: $10 per day per</p>
-        <p>Phone: (443) 515-7044</p>
-        <p>Email: warenoir@gmail.com</p>
+        <p>Phone: {props.listing.phone}</p>
+        <p>Email: {props.listing.email}</p>
 
     </div>
   )
